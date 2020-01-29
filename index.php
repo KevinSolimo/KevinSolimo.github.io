a2616ef9801087fae0db8c38c08b1026a6679532
KevinSolimo <45125040+KevinSolimo@users.noreply.github.com>
2020-01-29 16:49:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+// open the file in a binary mode
+
+//----------
+
+$name = 'https://github.com/KevinSolimo/CheckMailOpen/raw/master/XECURITY_LOGO.png';
+
+//----------
+
+$fp = fopen($name, 'rb');
+
+// send the right headers
+header("Content-Type: image/png");
+header("Content-Length: " . filesize($name));
+
+// dump the picture and stop the script
+fpassthru($fp);
+exit;
+
+?>
